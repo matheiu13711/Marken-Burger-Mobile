df23268fc9c80f36773fe31f5cafbef75c30a678
matheiu amiel perdido <qmavperdido@tip.edu.ph>
2022-10-20 00:24:57 +0800

    login+register
    no functionality yet

---
+++ src/app/register/register.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  isModalOpen = false;
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+}
