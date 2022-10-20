c692b9548464acc290ef4e9cdb4fb00debde6174
matheiu amiel perdido <qmavperdido@tip.edu.ph>
2022-10-21 01:01:38 +0800

    birthday form

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
+  loginForm: FormGroup = this.formBuilder.group({
+    email: [''],
+    password: [''],
+  })
   ngOnInit() {
   }
 
+  async onSubmit(){
+    let loginSubmit = {
+      'email' : this.loginForm.controls.email.value,
+      'password' : this.loginForm.controls.password.value,
+    }
+    
+    console.log(loginSubmit);
+    
+  }
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,20 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
+  providers: [DatePipe]
 })
 export class RegisterPage implements OnInit {
+  testBday: any;
+  testInp: any;
 
-  constructor() { }
+  registerInfo = {
+    email: 'aaaaa',
+    birthday: 'asd',
+  }
+  constructor(
+    private formBuilder: FormBuilder,
+    private datepipe: DatePipe
+    ) { }
 
   ngOnInit() {
+    this.testInp = '100 percent';
   }
+
+  registerForm: FormGroup = this.formBuilder.group({
+    email: [''],
+    firstName: [''],
+    middleName: [''],
+    lastName: [''],
+    birthdate: [''],
+    password: [''],
+    confirmPass: [''],
+  })
 
   isModalOpen = false;
 
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
+  }
+
+  dateChange(value){
+    this.testBday = value;
+    console.log(this.testBday);
+    
+  }
+
+  async onSubmit(){
+    let registerSubmit = {
+      'email': this.registerForm.controls.email.value,
+      'birthdate': this.registerForm.controls.birthdate.value,
+    }
+    console.log(registerSubmit);
+  }
+
+  test(value){
+    this.registerInfo.birthday = this.datepipe.transform(value, 'MMMM dd, YYYY');
   }
 }
