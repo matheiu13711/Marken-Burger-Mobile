668f427f1afe5f66378dba1f6c1f7d4a20c44f5b
Julian Signo <qjkcsigno@tip.edu.ph>
2022-10-25 14:22:53 +0800

    Contact update

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private navCtrl: NavController
+  ) { }
+
+  ContactForm: FormGroup = this.formBuilder.group({
+    senderName: ['', [Validators.required]],
+    senderEmail: ['', [Validators.required]],
+    senderMessage: ['', [Validators.required]],
+  })
 
   ngOnInit() {
+  }
+
+  async onSubmit(){
+    let loginSubmit = {
+      'senderName' : this.ContactForm.controls.senderName.value,
+      'senderEmail' : this.ContactForm.controls.senderEmail.value,
+      'senderMessage' : this.ContactForm.controls.senderMessage.value,
+    }
+    
+    console.log(loginSubmit);
   }
 
 }
